76881b7e202092ab2dafe0a654ce6061a4f7c76f
JrDuComptoirDesPharmacies <jeremy@lecomptoirdespharmacies.fr>
2024-03-01 15:19:24 +0100

    LDS-3428 NullPointerException: Cannot invoke "java.util.List.size()" because "objects" is null
    Fix: objects can be null

--- src/main/java/fr/lecomptoirdespharmacies/core/helpers/ListHelper.java
+++ src/main/java/fr/lecomptoirdespharmacies/core/helpers/ListHelper.java
@@ -2,19 +2,21 @@ package fr.lecomptoirdespharmacies.core.helpers;
 
 import fr.lecomptoirdespharmacies.entities.AbstractBase;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ListHelper {
 
     public static <T extends AbstractBase>  T getObject(List<T> objects) throws Exception{
+        if(Objects.isNull(objects) || objects.isEmpty())
+            return null;
+
         if(objects.size() == 1){
             return objects.get(0);
-        } else if (objects.size() > 1) {
+        } else {
             throw new Exception("Too much element for this request.");
         }
-        return null;
     }
 
     public static List<String> mergeList(List<String> list1, List<String> list2){

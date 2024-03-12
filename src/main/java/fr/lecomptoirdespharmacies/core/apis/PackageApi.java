d1f046e563194400cc2c7a9eab64dab58f60e8f9
Antoine LCDP <antoine@lecomptoirdespharmacies.fr>
2024-03-12 08:05:26 +0100

    LDS-3428 :
    Remove unused import

--- src/main/java/fr/lecomptoirdespharmacies/core/apis/PackageApi.java
+++ src/main/java/fr/lecomptoirdespharmacies/core/apis/PackageApi.java
@@ -10,7 +10,6 @@ import fr.lecomptoirdespharmacies.entities.Package;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URLEncoder;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
